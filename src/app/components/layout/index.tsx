42a653c99af27e5a5179252ba98226b98b6c19b1
Sergey Mishin <sergey.mishin.71@gmail.com>
2025-06-09 13:11:04 +0300

    router fix

--- src/app/components/layout/index.tsx
+++ src/app/components/layout/index.tsx
@@ -1,12 +1,15 @@
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 import '@cloudscape-design/global-styles/index.css';
 import { AutoBreadCrumbs } from '@/app/components/layout/components/auto-bread-crumbs';
 import { AppLayout } from '@cloudscape-design/components';
 import NavList from './components/nav-list';
 
 export default function Layout() {
+  const location = useLocation();
+
   return (
     <AppLayout
+      key={location.pathname}
       content={<Outlet />}
       navigation={<NavList />}
       breadcrumbs={<AutoBreadCrumbs />}
